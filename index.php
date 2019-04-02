5793d728e8b35f6811187f19b8eda8f519a39fb9
Bunnbuns <ben@bunnbuns.net>
2019-04-02 14:46:19 -0400

    new design in progress!

--- index.php
+++ index.php
@@ -16,54 +16,68 @@ include('inc/common.inc.php');
 <meta name="msapplication-TileColor" content="#455a64">
 <meta name="theme-color" content="#455a64">
     <!-- css -->
-    <link href="/static/css/mui.min.css" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="/static/css/materialize.min.css" rel="stylesheet" type="text/css" />
     <link href="/static/css/main.css" rel="stylesheet" type="text/css" />
 <style>
-
+*{
+    -webkit-font-smoothing: antialiased;
+}
 </style>
 </head>
 <body>
-<div id="content-wrapper">
-    <div class="this">
+  <nav class=" blue darken-3" role="navigation">
+    <div class="nav-wrapper container">
+      <a id="logo-container" href="#" class="brand-logo">Pages</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#">Login</a></li>
+      </ul>
 
-<div class="hero">
-    <div class="mui--appbar-height"></div>
-  <h1 class="hero-text"><center>Pages</center></h1>
-</div>
-<div class="page-content">
-  <div class="pages">
-      <div class="note-light yellow" style="margin-bottom:.5rem;font-weight:500;">Pages is not completed! Check back later.</div>
-
-      <a style="text-decoration:none;" href="#">
-    <div class="page">
-      <div class="content">
-      <h2>Title</h2>
-        <p>Thigngs</p></div>
-      <div class="page-bottom">
-        <img class="pfp" src="http://topgunurl.tk/pages/pfp/Bunnbuns.png">
-        <span class="pfp-text">Ben B <span class="pfp-date">
-                <span class="tag">Tag 1</span>
-                <span class="tag">Tag 2</span>
-            </span></span></div>
-    </div></a>
-
- <!--     
-    <div class="page">
-      <div class="content">
-      <h2>This is a page</h2>
-        <p>A description about this page for you to read and enjoy...</p></div>
-      <div class="page-bottom">
-        <img class="pfp" src="https://benworld.net/protected/u/default.png">
-        <span class="pfp-text">A_Guy <span class="pfp-date">
-                <span class="tag">First</span><span class="tag">Page</span>
-            </span></span></div>
+      <ul id="nav-mobile" class="sidenav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Login</a></li>
+        <li><a href="#">Sign Up</a></li>
+      </ul>
+      <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
     </div>
--->
+  </nav>
+<div class="hero blue lighten-4">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <h1 class="header center blue-text text-darken-3">Pages</h1>
+        <div class="row center">
+          <h5 class="header col s12 light">A place to share information with pages and comments</h5>
+        </div>
+        <div class="row center">
+          <a href="#" id="download-button" class="btn-large waves-effect waves-light blue darken-3">Get Started</a>
+        </div>
+        <br><br><br>
 
+      </div>
+    </div>
   </div>
-</div>  
-  
-  </div><!-- End .this -->
+<div class="container">
+  <h5>Latest pages</h5>
+    <div class="this">
+        <div class="page">
+          <div class="content">
+          <h5>This is a page</h5>
+            <p>A description about this page for you to read and enjoy...</p></div>
+          <div class="page-bottom">
+            <img class="pfp" style="max-height:50px;" src="https://benworld.net/protected/u/default.png">
+            <span class="pfp-text">A_Guy <span class="pfp-date">
+                    <span class="tag">First</span><span class="tag">Page</span>
+                </span></span></div>
+        </div>
+    </div>
 </div>
+
+<script type="text/javascript" src="/static/js/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="/static/js/materialize.min.js"></script>
+<script>
+ $(document).ready(function(){
+    $('.sidenav').sidenav();
+  }); 
+</script>
 </body>
 </html>
