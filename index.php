b1083ea0069fe2573a08d06206d7e040af4607c5
Bunnbuns <ben@bunnbuns.net>
2019-04-02 10:16:27 -0400

    added favicons

--- index.php
+++ index.php
@@ -9,7 +9,10 @@ include('inc/common.inc.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Pages</title>
-
+<link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png">
+<link rel="manifest" href="/static/favicon/site.webmanifest">
 <meta name="msapplication-TileColor" content="#455a64">
 <meta name="theme-color" content="#455a64">
     <!-- css -->
