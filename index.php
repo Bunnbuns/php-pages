b73ca5f9836aa23650dce7d74fbd7b36394d2dcb
Bunnbuns <ben@bunnbuns.net>
2019-04-03 07:35:04 -0400

    changed the inputs

--- index.php
+++ index.php
@@ -63,11 +63,11 @@ include('inc/common.inc.php');
       <div class="row">
         <div class="input-field col s6">
           <input id="first_name" type="text" class="validate">
-          <label for="first_name">First Name</label>
+          <label for="first_name">Full Name</label>
         </div>
         <div class="input-field col s6">
           <input id="last_name" type="text" class="validate">
-          <label for="last_name">Last Name</label>
+          <label for="last_name">Username</label>
         </div>
       </div>
       <div class="row">
