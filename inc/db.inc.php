5cbdea3b5152b449a0ef8109432c4ec9551ba2fd
Ben Border <ben@bunnbuns.net>
2019-04-09 19:58:58 -0400

    Update db.inc.php

--- inc/db.inc.php
+++ inc/db.inc.php
@@ -1,8 +1,8 @@
 <?php
-$host       = "sql135.main-hosting.eu.";
-$username   = "u611188077_pages";
-$password   = "jboedJ1&41h~~!++|o";
-$dbname     = "u611188077_pages";
+$host       = "localhost";
+$username   = "username";
+$password   = "password";
+$dbname     = "dbname";
 $dsn        = "mysql:host=$host;dbname=$dbname";
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
