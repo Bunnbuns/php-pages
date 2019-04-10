443443d21bcb99fa33810523e6831b07e7b7d907
Bunnbuns <ben@bunnbuns.net>
2019-04-10 13:56:12 -0400

    fixed sidebar links

--- inc/navbar.inc.php
+++ inc/navbar.inc.php
@@ -7,9 +7,9 @@
       </ul>
 
       <ul id="nav-mobile" class="sidenav">
-        <li class="active"><a href="#">Home</a></li>
+        <li class="active"><a href="/">Home</a></li>
         <li><a href="/login/">Login</a></li>
-        <li><a href="#">Sign Up</a></li>
+        <li><a href="/">Sign Up</a></li>
       </ul>
       <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
     </div>
